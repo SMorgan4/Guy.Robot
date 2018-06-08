42380c613e2080f715b8af60ea1537cf7b52c10a
Scottmorgan <22440249SMorgan4@users.noreply.github.com>
2018-06-08 13:45:35 -0700

    Added response classes

--- UI.py
+++ UI.py
@@ -1,5 +1,6 @@
 import weakref
 import asyncio
+import discord
 
 
 class UI:
@@ -7,7 +8,8 @@ class UI:
      names as a tuple to create a custom layout. Supported elements are: min, max, close. """
     def __init__(self, parent, element_list=('max', 'min', 'close')):
         self.parent = weakref.ref(parent)
-        self.standard_elements = {'max': ('➕', self.maximize), 'min': ('➖', self.minimize), 'close': ('✖', self.close)}
+        self.standard_elements = {'max': ('➕', self.maximize), 'min': ('➖', self.minimize), 'close': ('✖', self.close),
+                                  'help': ('❓', self.help)}
         self.elements = {}
         self.set_elements(element_list)
 
@@ -58,3 +60,73 @@ class UI:
         parent().update_size('max')
         await parent().bot_message.edit(embed=self.parent().embed)
         return True
+
+    async def help(self, parent):
+        await parent().help()
+
+# Standard response classes
+
+
+class UIResponse:
+    """Generic class for embedded message with UI"""
+    def __init__(self, ctx, message):
+        self.bot_message = None
+        self.ui = UI(self)
+        self.embed = message
+        self.ctx = ctx
+        self.user_message = ctx.message
+
+    async def send(self):
+        """Sends message and starts UI"""
+        self.bot_message = await self.ctx.send(embed=self.embed)
+        await self.ui.start(self.ctx.bot)
+
+
+class CloseableResponse(UIResponse):
+    """Class for user closable embedded bot messages"""
+    def __init__(self, ctx, message):
+        UIResponse.__init__(self, ctx, message)
+        self.ui = UI(self, element_list=["close"])
+
+
+class ResizeableResponse(UIResponse):
+    """Preview object creates and manages an embed object if supplied with a valid forum link"""
+    def __init__(self, ctx, message, settings, size="std"):
+        UIResponse.__init__(self, ctx, message)
+        self.settings = settings
+        self.size = size
+        self.lines = []
+        self.embed_text = ''
+
+    def get_lines(self, content):
+        """Breaks the messages content into a series of lines roughly corresponding with one line of text on mobile"""
+        while content != '':
+            new_line = content.split('\n', 1)[0] + '\n'
+            if len(new_line) > self.settings.line_length:
+                new_line = content[:self.settings.line_length-1]
+                content = content[len(new_line):]
+            else:
+                content = content[len(new_line):]
+            self.lines.append(str(new_line))
+
+    def no_lines(self):
+        """Returns the number of lines for a given setting"""
+        if self.size == 'max':
+            no = self.settings.max_lines
+        else:
+            no = self.settings.std_lines
+        if no > len(self.lines):
+            no = len(self.lines)
+        return no
+
+    def select_lines(self):
+        """Selects the specified number of lines places them in embed_text"""
+        self.embed_text = ''
+        for line in range(0, self.no_lines()):
+            self.embed_text += self.lines[line]
+
+    def update_size(self, size):
+        """Updates the size of the embed and rebuilds it"""
+        if self.size != size:
+            self.size = size
+            self.embed.description =  self.embed_text
